11ee01159ccccc39b71f8cb888a52b3d518e280b
Atom2033 <140852498+Atom2033@users.noreply.github.com>
2023-11-19 23:59:12 +0300

    Update RSA.java

--- RSA.java
+++ RSA.java
@@ -20,4 +20,17 @@ public class RSA {
 
    public  long[] encrypt(String message, int e, int n)
 }
+ static String intArrayToString(int[] a) {
+        StringBuilder string = new StringBuilder();
+        for (int i = 0; i < a.length; i++) {
+            string.append(intToString(a[i]));
+        }
+        return string.toString();
+    }
+
+    static String intToString(int n) {
+        char alphabetChar = (char) ('A' + n - 1);
+        return String.valueOf(alphabetChar);
+    }
+}
 
