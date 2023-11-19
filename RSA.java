122291d44dd64a469d2a831793d1a0ed0375cbcc
Yaraalfouzan <alfouzan.yara@gmail.com>
2023-11-19 12:42:39 +0300

    start

---
+++ RSA.java
@@ -0,0 +1,14 @@
+public class RSA {
+
+    public int[] LCG (int seed,int quantity){
+
+    }
+
+    public boolean millerRabinTest(long n,int k){
+
+    }
+
+    public int[] String_to_intArray(String n){
+
+    }
+}
