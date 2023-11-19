f6b21ed893144318ce42756824b5476b46a11974
NoufAlwakiyel <142484727+NoufAlwakiyel@users.noreply.github.com>
2023-11-20 00:38:32 +0300

    Updated
    ;)

--- RSA.java
+++ RSA.java
@@ -13,7 +13,39 @@ public class RSA {
     }
 
 
-   public long extendedEuclideanAlgorithm(long e, long m){
+   public long extendedEuclideanAlgorithm(long a, long m){
+    // we are trying to find the inverse of ((a)) mod m, s.a a=dq+r
+    
+    int m0 = m;  // Store the original value of 'm' 
+
+    // Initialize 'x' and 'y' for the iterative process
+    int x = 1, y = 0;
+
+    // Iterate using the Extended Euclidean Algorithm until 'a' becomes 1 ,
+            while (a > 1) {
+        int q = a / m;  // Calculate quotient 'q' 
+
+        int t = m;      // Store the current value of 'm' 
+
+        // Update 'm' and 'a' for the (next) iteration
+        m = a % m;
+        a = t;
+
+        t = y;          // Store the current value of 'y'
+
+        // Update 'y' and 'x' based on the (current) iterative formula
+        y = x - q * y; 
+        x = t;
+    }
+
+    if (x < 0)
+        x += m0;        // If 'x' is negative, convert it to its positive equivalent
+
+    if (x == 0)
+        return 0;       // Return 0 if the modular inverse doesn't exist
+    else
+        return x;       // Return the modular multiplicative inverse 'x'
+
 
    }
 
@@ -32,5 +64,6 @@ public class RSA {
         char alphabetChar = (char) ('A' + n - 1);
         return String.valueOf(alphabetChar);
     }
-}
+
+
 
